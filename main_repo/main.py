9f236a80b73c3aa882aeb3fa10d05c8c9876d638
ashokreddy123 <ashokreddy.venna143@gmail.com>
2021-07-12 21:30:46 +0530

    Update main.py

--- main_repo/main.py
+++ main_repo/main.py
@@ -58,7 +58,7 @@ classes = ('plane', 'car', 'bird', 'cat', 'deer',
 # Model
 print('==> Building model..')
 # net = VGG('VGG19')
-net = ResNet18()
+#net = ResNet18()
 # net = PreActResNet18()
 # net = GoogLeNet()
 # net = DenseNet121()
@@ -71,7 +71,7 @@ net = ResNet18()
 # net = ShuffleNetV2(1)
 # net = EfficientNetB0()
 # net = RegNetX_200MF()
-# net = SimpleDLA()
+net = SimpleDLA()
 net = net.to(device)
 if device == 'cuda':
     net = torch.nn.DataParallel(net)
